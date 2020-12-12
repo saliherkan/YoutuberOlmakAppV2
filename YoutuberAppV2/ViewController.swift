6ba3b7642843948a088ee47e901053cc66db5dfd
Salih Erkan SANDAL <saliherkansandal@gmail.com>
2020-12-12 15:34:01 +0300

    new project Strat

---
+++ YoutuberAppV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutuberAppV2
+//
+//  Created by Sefer Furkan Sandal on 12.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
